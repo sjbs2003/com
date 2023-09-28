0992bc542fff7a796a01d8935b34f161bf4cd340
Suraj Barik <surajbarik2003@gmail.com>
2023-09-28 23:03:55 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ sort/BestSort.java
@@ -0,0 +1,13 @@
+package com.sort;
+
+import java.util.Arrays;
+
+public class BestSort {
+    public static void main(String[] args) {
+        int[] array = {2,1,4,5,6,77,4,88,2,4,3243};
+
+        Arrays.sort(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+}
