7120b135e6cd94b05ae58dfc478f96d2d935eba9
Suraj Barik <surajbarik2003@gmail.com>
2023-10-10 22:30:54 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Stack_And_Queue/Practice.java
@@ -0,0 +1,41 @@
+package com.Stack_And_Queue;
+
+public class Practice {
+
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
+    int ptr = -1;
+
+    public Practice(){
+        this(DEFAULT_SIZE);
+    }
+
+    public Practice(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean push(int item){
+        if (isFull()){
+            System.out.println("Stack is Full");
+            return false;
+        }
+        ptr++;
+        data[ptr] =item;
+        return true;
+    }
+
+    public int remove() throws Exception{
+        if (isEmpty()){
+            throw new Exception("Stack is Empty");
+        }
+        return data[ptr--];
+    }
+
+    public boolean isFull(){
+       return ptr == data.length-1;
+    }
+    public boolean isEmpty(){
+        return ptr == -1;
+    }
+}
+
